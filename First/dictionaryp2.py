02fff4754db826460bfdb5086f0acfcb258bcceb
Ilya <ilya.poloshovets@gmail.com>
2022-10-30 13:54:20 +0300

    Словари 2 часть

---
+++ First/dictionaryp2.py
@@ -0,0 +1,17 @@
+price = {'meat': 2, 'bread': 1, 'potato': 0.5, 'water': 0.2}
+
+
+# for i in price:
+#     price[i] = round(price[i] * 0.85, 2)
+
+# new = {}
+
+# for key, value in price.items():
+#     new[value] = key
+# print(new)
+
+# for value in price.values():
+#     print(value)
+
+# for key in price.keys():
+#     print(key)
