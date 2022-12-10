0109f3ffb45e6c0024f21eb169facf95b00ce056
Илья <ilya.poloshovets@gmail.com>
2022-12-10 16:18:14 +0300

    Stepik 2.5

---
+++ Stepik/PrintInput.py
@@ -0,0 +1,40 @@
+# # Вывод последней и первой цифры двузначного числа
+# num = 17
+# a = num % 10
+# b = num // 10
+#
+# # print(a)
+# # print(b)
+#
+# # Вывод цифр трехзначного числа
+#
+# num = 754
+# a = num % 10
+# b = (num % 100) // 10
+# c = num // 100
+# print(a)
+# print(b)
+# print(c)
+
+# num = int(input())
+# a = num % 10
+# b = (num % 100) // 10
+# c = num // 100
+#
+# summ = a + b + c
+# mul = a * b * c
+#
+# print('Сумма цифр =', summ)
+# print('Произведение цифр =', mul)
+
+num = int(input())
+a = num % 10  # c
+b = (num % 100) // 10  # b
+c = num // 100  # a
+
+print(str(c) + str(b) + str(a))  # abc
+print(str(c) + str(a) + str(b))  # acb
+print(str(b) + str(c) + str(a))  # bac
+print(str(b) + str(a) + str(c))  # bca
+print(str(a) + str(c) + str(b))  # cab
+print(str(a) + str(b) + str(c))  # cba
