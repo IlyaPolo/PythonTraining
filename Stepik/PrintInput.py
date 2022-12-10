704655f2b415e22907fa2e8e105e1eb023c929a9
Илья <ilya.poloshovets@gmail.com>
2022-12-10 16:39:18 +0300

    Stepik 2.5

--- Stepik/PrintInput.py
+++ Stepik/PrintInput.py
@@ -27,14 +27,25 @@
 # print('Сумма цифр =', summ)
 # print('Произведение цифр =', mul)
 
-num = int(input())
-a = num % 10  # c
-b = (num % 100) // 10  # b
-c = num // 100  # a
+# num = int(input())
+# a = num % 10  # c
+# b = (num % 100) // 10  # b
+# c = num // 100  # a
+#
+# print(str(c) + str(b) + str(a))  # abc
+# print(str(c) + str(a) + str(b))  # acb
+# print(str(b) + str(c) + str(a))  # bac
+# print(str(b) + str(a) + str(c))  # bca
+# print(str(a) + str(c) + str(b))  # cab
+# print(str(a) + str(b) + str(c))  # cba
 
-print(str(c) + str(b) + str(a))  # abc
-print(str(c) + str(a) + str(b))  # acb
-print(str(b) + str(c) + str(a))  # bac
-print(str(b) + str(a) + str(c))  # bca
-print(str(a) + str(c) + str(b))  # cab
-print(str(a) + str(b) + str(c))  # cba
+num = int(input())
+a = num % 10
+b = (num % 100) // 10
+c = (num % 1000) // 100
+d = num // 1000
+
+print('Цифра в позиции тысяч равна', d)
+print('Цифра в позиции сотен равна', c)
+print('Цифра в позиции десятков равна', b)
+print('Цифра в позиции единиц равна', a)
